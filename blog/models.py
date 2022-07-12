5bf63783e7cbf0b9a3bd9888258999bd62586070
Camden Navis <naviscam@gmail.com>
2022-07-12 16:29:28 -0400

    initial commit

---
+++ blog/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.db.models.fields import DateTimeField
+from django.utils import timezone
+from datetime import datetime
+
+
+class Post(models.Model):
+    author = models.ForeignKey("auth.User", on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateField(default=timezone.now)
+    published_date = models.DateField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
